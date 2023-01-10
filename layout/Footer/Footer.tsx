00c75b385b5c98eb4e9090c31b06b468b9e10a01
Igor <azawertik@gmail.com>
2023-01-11 00:21:05 +0300

    added footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { IFooterProps } from "./Footer.props";
-
+import styles from "./Footer.module.scss";
 const Footer = ({ ...props }: IFooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+  return (
+    <div {...props} className={styles.wrapper}>
+      <span className={styles.left_side}>
+        OwlTop © 2020 - 2021 Все права защищены
+      </span>
+      <div className={styles.right_side}>
+        <span>Пользовательское соглашение</span>
+        <span>Политика конфиденциальности</span>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
