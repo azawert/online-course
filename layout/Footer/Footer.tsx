bfbef61d2f74617d61453a2cee08b7baadda38c4
Igor <azawertik@gmail.com>
2023-01-16 23:06:33 +0300

    fixed layout footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,18 +1,12 @@
 import React from "react";
-import { IFooterProps } from "./Footer.props";
-import styles from "./Footer.module.scss";
-const Footer = ({ ...props }: IFooterProps): JSX.Element => {
-  return (
-    <div {...props} className={styles.wrapper}>
-      <span className={styles.left_side}>
-        OwlTop © 2020 - 2021 Все права защищены
-      </span>
-      <div className={styles.right_side}>
-        <span>Пользовательское соглашение</span>
-        <span>Политика конфиденциальности</span>
-      </div>
-    </div>
-  );
+import {IFooterProps} from "./Footer.props";
+
+const Footer = ({...props}: IFooterProps): JSX.Element => {
+    return (
+        <div {...props}>
+            footer
+        </div>
+    );
 };
 
 export default Footer;
